c55f6b1e7b222bad7b38def03fab9f0a9afe2498
gabrielcarloslopes <gabriel.lopes@solutiosoftware.com.br>
2022-12-12 20:36:32 -0300

    ensure HttpAdapter throws ServerError if post throws

--- clean_arch_study/test/data/infra/http/http_adapter_test.dart
+++ clean_arch_study/test/data/infra/http/http_adapter_test.dart
@@ -39,6 +39,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -123,6 +127,13 @@ void main() {
     });
     test('Should return ServerError with post returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+    test('Should return ServerError with post throws', () async {
+      mockError();
 
       final future = sut.request(url: url, method: 'post');
 
