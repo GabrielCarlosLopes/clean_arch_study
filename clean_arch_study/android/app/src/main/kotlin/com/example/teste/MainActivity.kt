2b56471786b17e8c839201236ed0938b2714f26d
gabrielcarloslopes <gabriel.lopes@solutiosoftware.com.br>
2022-12-13 21:19:43 -0300

    add LoginPage

--- clean_arch_study/android/app/src/main/kotlin/com/example/teste/MainActivity.kt
+++ clean_arch_study/android/app/src/main/kotlin/com/example/teste/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.clean_arch_study
+package com.example.teste
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ clean_arch_study/lib/main/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/Material.dart';
+
+import '../ui/components/components.dart';
+
+void main() {
+  runApp(App());
+}
