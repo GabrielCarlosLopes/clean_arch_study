9ff3294ad2383680e5adde67c7a5fb8f5e01a442
gabrielcarloslopes <gabriel.lopes@solutiosoftware.com.br>
2022-12-15 07:35:10 -0300

    move auth mock to a helper method

--- clean_arch_study/test/presentation/presenters/stream_login_presenter_test.dart
+++ clean_arch_study/test/presentation/presenters/stream_login_presenter_test.dart
@@ -39,10 +39,19 @@ void main() {
   StreamLoginPresenter sut;
   ValidationSpy validation;
   String email;
+
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field ?? anyNamed('field'), value: anyNamed('value')));
+
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
   setUp(() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    mockValidation();
   });
   test('should call validation with correct email', () {
     sut.validateEmail(email);
@@ -50,13 +59,7 @@ void main() {
     verify(validation.validate(field: 'email', value: email)).called(1);
   });
   test('should emit email error if validation fails', () {
-    when(
-      validation.validate(
-        field: anyNamed('field'),
-        value: anyNamed('value'),
-      ),
-    ).thenReturn('error');
-
+    mockValidation(value: 'error');
     expectLater(sut.emailErrorStream, emits('error'));
 
     sut.validateEmail(email);
