badfbca66c4d38b00063c08972986ed9d406941b
gabriellopessolutio <gabriel.lopes@solutiosoftware.com.br>
2022-12-16 08:43:01 -0300

    ensure StreamLoginPresenter calls auth with correct values

--- clean_arch_study/lib/presentation/presenters/stream_login_presenter.dart
+++ clean_arch_study/lib/presentation/presenters/stream_login_presenter.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+
 import 'package:meta/meta.dart';
 
-import 'package:ForDev/presentation/protocols/protocols.dart';
+import '../../domain/usecases/usecases.dart';
+
+import '../protocols/protocols.dart';
 
 class LoginState {
   String email;
@@ -18,6 +21,7 @@ class LoginState {
 
 class StreamLoginPresenter {
   final Validation validation;
+  final Authentication authentication;
   final _controller = StreamController<LoginState>.broadcast();
 
   var _state = LoginState();
@@ -29,7 +33,8 @@ class StreamLoginPresenter {
   Stream<bool> get isFormValid =>
       _controller.stream.map((state) => state.isFormValid).distinct();
 
-  StreamLoginPresenter({@required this.validation});
+  StreamLoginPresenter(
+      {@required this.validation, @required this.authentication});
 
   void update() => _controller.add(_state);
 
@@ -44,5 +49,11 @@ class StreamLoginPresenter {
     _state.passwordError =
         validation.validate(field: 'password', value: password);
     update();
+  }
+
+  Future<void> auth() async {
+    await authentication.auth(
+      AuthenticationParams(email: _state.email, secret: _state.password),
+    );
   }
 }
--- clean_arch_study/test/presentation/presenters/stream_login_presenter_test.dart
+++ clean_arch_study/test/presentation/presenters/stream_login_presenter_test.dart
@@ -2,16 +2,21 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:ForDev/domain/usecases/authentication.dart';
+
 import 'package:ForDev/presentation/protocols/protocols.dart';
 import 'package:ForDev/presentation/presenters/presenters.dart';
 
 class ValidationSpy extends Mock implements Validation {}
+
+class AuthenticationSpy extends Mock implements Authentication {}
 
 void main() {
   StreamLoginPresenter sut;
   ValidationSpy validation;
   String email;
   String password;
+  AuthenticationSpy authentication;
 
   PostExpectation mockValidationCall(String field) => when(validation.validate(
       field: field ?? anyNamed('field'), value: anyNamed('value')));
@@ -22,7 +27,9 @@ void main() {
 
   setUp(() {
     validation = ValidationSpy();
-    sut = StreamLoginPresenter(validation: validation);
+    authentication = AuthenticationSpy();
+    sut = StreamLoginPresenter(
+        validation: validation, authentication: authentication);
     email = faker.internet.email();
     password = faker.internet.password();
     mockValidation();
@@ -92,5 +99,15 @@ void main() {
     sut.validateEmail(email);
     await Future.delayed(Duration.zero);
     sut.validatePassword(password);
+  });
+  test('should call atuh with correct values', () async {
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    await sut.auth();
+
+    verify(authentication
+            .auth(AuthenticationParams(email: email, secret: password)))
+        .called(1);
   });
 }
