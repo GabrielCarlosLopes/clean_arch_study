e1b501d467b02ba21f8eba1796ca552881abdbcd
--global <--global>
2022-12-21 09:39:20 -0300

    ensure EmailValidation return error if email is invalid

--- clean_arch_study/test/validation/validators/email_validation_test.dart
+++ clean_arch_study/test/validation/validators/email_validation_test.dart
@@ -7,7 +7,10 @@ class EmailValidation implements FieldValidtion {
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido';
   }
 }
 
@@ -26,5 +29,8 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('gabrielcarloslopes.dev@gmail.com'), null);
+  });
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('gabrielcarloslopes.dev'), 'Campo inválido');
   });
 }
