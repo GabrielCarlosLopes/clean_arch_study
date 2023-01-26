0a706c3b082b93703b80fa58aaeb873480fdf257
gabrielcarloslopes <gabriel.lopes@solutiosoftware.com.br>
2023-01-26 17:54:36 -0300

    move production code to their own files

---
+++ clean_arch_study/lib/data/cache/save_secure_cache_storage.dart
@@ -0,0 +1,3 @@
+abstract class SaveSecureCacheStorage {
+  Future<void> saveSecure({String key, String value});
+}
--- clean_arch_study/test/data/usecases/save_current_account/save_current_account_test.dart
+++ clean_arch_study/test/data/usecases/save_current_account/save_current_account_test.dart
@@ -1,30 +1,12 @@
-import 'package:ForDev/domain/helpers/helpers.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'package:ForDev/domain/entities/entities.dart';
-import 'package:ForDev/domain/usecases/usecases.dart';
+import 'package:ForDev/domain/helpers/helpers.dart';
 
-class LocalSaveCurrentAccount implements SaveCurrentAccount {
-  final SaveSecureCacheStorage saveSecureCacheStorage;
-
-  LocalSaveCurrentAccount({this.saveSecureCacheStorage});
-  Future<void> save(AccountEntity account) async {
-    try {
-      await saveSecureCacheStorage.saveSecure(
-        key: 'token',
-        value: account.token,
-      );
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage {
-  Future<void> saveSecure({String key, String value});
-}
+import 'package:ForDev/data/cache/cache.dart';
+import 'package:ForDev/data/usecases/usecases.dart';
 
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {
 }
